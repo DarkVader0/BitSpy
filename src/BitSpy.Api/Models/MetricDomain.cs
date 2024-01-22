1ba80d37bf0c7d3f76b0180aee555665bd24ebf7
DarkVader <radepantelej@gmail.com>
2024-01-22 13:11:08 +0100

    .

--- src/BitSpy.Api/Models/MetricDomain.cs
+++ src/BitSpy.Api/Models/MetricDomain.cs
@@ -4,18 +4,18 @@ public class MetricDomain
 {
     public required string Name { get; init; }
     public required decimal TimeInGCSinceLastGCPercentage { get; init; }
-    public required ulong AllocationRatePerSecond { get; init; }
+    public required long AllocationRatePerSecond { get; init; }
     public required decimal CPUUsage { get; init; }
-    public required uint ExceptionCount { get; init; }
-    public required uint Gen0CollectionCount { get; init; }
-    public required ulong Gen0Size { get; init; }
-    public required uint Gen1CollectionCount { get; init; }
-    public required ulong Gen1Size { get; init; }
-    public required uint Gen2CollectionCount { get; init; }
-    public required ulong Gen2Size { get; init; }
-    public required uint ThreadPoolCompletedItemsCount { get; init; }
-    public required uint ThreadPoolQueueLength { get; init; }
-    public required uint ThreadPoolThreadCount { get; init; }
-    public required uint WorkingSet { get; init; }
+    public required long ExceptionCount { get; init; }
+    public required long Gen0CollectionCount { get; init; }
+    public required long Gen0Size { get; init; }
+    public required long Gen1CollectionCount { get; init; }
+    public required long Gen1Size { get; init; }
+    public required long Gen2CollectionCount { get; init; }
+    public required long Gen2Size { get; init; }
+    public required long ThreadPoolCompletedItemsCount { get; init; }
+    public required long ThreadPoolQueueLength { get; init; }
+    public required long ThreadPoolThreadCount { get; init; }
+    public required long WorkingSet { get; init; }
     public required DateTime Timestamp { get; init; }
 }
