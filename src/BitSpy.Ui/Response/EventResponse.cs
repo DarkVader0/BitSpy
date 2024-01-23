886ada497508a187b2dd4b60deb15b18f3914678
DarkVader <radepantelej@gmail.com>
2024-01-23 11:17:03 +0100

    .

---
+++ src/BitSpy.Ui/Response/EventResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BitSpy.Ui.Response;
+
+public sealed class EventResponse
+{
+    public required string Name { get; init; }
+    public required string Message { get; init; }
+    public required DateTime Timestamp { get; init; }
+    public required List<AttributeResponse> Attributes { get; init; } = new();
+}
---
+++ src/BitSpy.Ui/Response/TraceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BitSpy.Ui.Response;
+
+public sealed class TraceResponse
+{
+    public required string Name { get; init; }
+    public required DateTime StartTime { get; init; }
+    public required DateTime EndTime { get; init; }
+    public List<AttributeResponse> Attributes { get; init; } = new();
+    public List<EventResponse> Events { get; init; } = new();
+}
