98bbc462ad52ad8125b5851f4e9e48ecd2989103
DarkVader <radepantelej@gmail.com>
2024-02-01 13:39:00 +0100

    .

--- src/BitSpy.Ui/Response/EventResponse.cs
+++ src/BitSpy.Ui/Response/EventResponse.cs
@@ -2,7 +2,7 @@
 
 public sealed class EventResponse
 {
-    public required string Name { get; init; }
+    public required string Name { get; set; }
     public required string Message { get; init; }
     public required long Duration { get; init; }
     public required List<AttributeResponse> Attributes { get; init; } = new();
--- src/BitSpy.Ui/Response/TraceResponse.cs
+++ src/BitSpy.Ui/Response/TraceResponse.cs
@@ -2,7 +2,7 @@
 
 public sealed class TraceResponse
 {
-    public required string Name { get; init; }
+    public required string Name { get; set; }
     public required long Duration { get; init; }
     public List<AttributeResponse> Attributes { get; init; } = new();
     public List<TraceEventRelationshipResponse> Events { get; init; } = new();
